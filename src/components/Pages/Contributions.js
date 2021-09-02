d2a8be3a8a55dbc489b658e59c82c23a333dcb86
thatblindgeye <thatblindgeye@gmail.com>
2021-09-02 15:40:52 -0400

    Add components for contributions

--- src/components/Pages/Contributions.js
+++ src/components/Pages/Contributions.js
@@ -1,9 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ContributionCard from '../Cards/ContribCard';
+import work from '../../work.json';
 
 export default function Contributions() {
+  const [contributions, setContributions] = useState([]);
+
   useEffect(() => {
     document.title = 'Contributions | thatblindgeye';
   }, []);
 
-  return <div>Soon to be contributions page</div>;
+  useEffect(() => {
+    setContributions(work.contributions);
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {contributions.map((contribution, index) => {
+          return <ContributionCard key={index} contribution={contribution} />;
+        })}
+      </div>
+    </div>
+  );
 }
