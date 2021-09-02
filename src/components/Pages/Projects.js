ae7452481ed3dfaa88fa935c84c14212b0dcf6ab
thatblindgeye <thatblindgeye@gmail.com>
2021-09-02 15:19:00 -0400

    Add components for projects

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,9 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ProjectCard from '../Cards/ProjectCard';
+import work from '../../work.json';
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
+
   useEffect(() => {
     document.title = 'Projects | thatblindgeye';
   }, []);
 
-  return <div>Soon to be projects page</div>;
+  useEffect(() => {
+    setProjects(work.projects);
+  }, []);
+
+  return (
+    <div>
+      {projects.map((project, index) => {
+        return <ProjectCard key={index} project={project} />;
+      })}
+    </div>
+  );
 }
