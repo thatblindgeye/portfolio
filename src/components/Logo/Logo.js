0952ca447fcd0962f5724afc1ffa474dfb710851
thatblindgeye <thatblindgeye@gmail.com>
2021-09-15 15:27:03 -0400

    Add dice rolling to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as EyeLogo } from '../../assets/images/logos/eye20-empty.svg';
 import { ReactComponent as D20 } from '../../assets/images/logos/d20.svg';
 
 export default function Logo() {
+  const [dice, setDice] = useState(20);
+  const [roll, setRoll] = useState(null);
+
+  const handleDiceRoll = (e) => {
+    e.stopPropagation();
+    const randomNumber = Math.floor(Math.random() * dice + 1);
+
+    setRoll(randomNumber);
+  };
+
   return (
     <div className='logo-container'>
-      <D20 />
-      <EyeLogo />
+      <button
+        className='logo-button'
+        onClick={handleDiceRoll}
+        tabIndex='0'
+        aria-label='Roll the d20'
+      >
+        {!roll ? (
+          <D20 aria-hidden='true' />
+        ) : (
+          <div aria-label={`You rolled ${roll}`} className='dice__result'>
+            {roll}
+          </div>
+        )}
+        <EyeLogo aria-hidden='true' />
+      </button>
     </div>
   );
 }
