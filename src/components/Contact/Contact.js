871cf89b639b6bfd7c3fc0c20b3ffc6b158d7f56
Eric Olkowski <git.eric@thatblindgeye.dev>
2025-11-16 16:48:16 -0500

    Updated email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,28 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
-import React from 'react';
-import githubDark from '../../assets/images/logos/GitHub-White-Mark-64px.png';
-import githubLight from '../../assets/images/logos/GitHub-Black-Mark-64px.png';
-import linkedinDark from '../../assets/images/logos/linkedin-white.png';
-import linkedinLight from '../../assets/images/logos/linkedin-blue.png';
-import emailDark from '../../assets/images/icons/email_white_24dp.png';
-import emailLight from '../../assets/images/icons/email_black_24dp.png';
+import React from "react";
+import githubDark from "../../assets/images/logos/GitHub-White-Mark-64px.png";
+import githubLight from "../../assets/images/logos/GitHub-Black-Mark-64px.png";
+import linkedinDark from "../../assets/images/logos/linkedin-white.png";
+import linkedinLight from "../../assets/images/logos/linkedin-blue.png";
+import emailDark from "../../assets/images/icons/email_white_24dp.png";
+import emailLight from "../../assets/images/icons/email_black_24dp.png";
 
 export default function Contact({ theme }) {
   const contacts = [
     {
-      url: 'https://github.com/thatblindgeye',
-      icon: theme === 'dark' ? githubDark : githubLight,
-      alt: 'GitHub',
+      url: "https://github.com/thatblindgeye",
+      icon: theme === "dark" ? githubDark : githubLight,
+      alt: "GitHub",
     },
     {
-      url: 'https://www.linkedin.com/in/ericolkowski/',
-      icon: theme === 'dark' ? linkedinDark : linkedinLight,
-      alt: 'LinkedIn',
+      url: "https://www.linkedin.com/in/ericolkowski/",
+      icon: theme === "dark" ? linkedinDark : linkedinLight,
+      alt: "LinkedIn",
     },
     {
-      url: 'mailto:ejo10488@gmail.com',
-      icon: theme === 'dark' ? emailDark : emailLight,
-      alt: 'Email',
+      url: "mailto:eric.contact@thatblindgeye.dev",
+      icon: theme === "dark" ? emailDark : emailLight,
+      alt: "Email",
     },
   ];
 
